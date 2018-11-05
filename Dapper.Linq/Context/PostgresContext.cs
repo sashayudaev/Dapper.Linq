8ddd5daba73bf5317248b023809981b7e81aec7e
Sasha <sashayudaev6@gmail.com>
2018-11-05 10:41:58 +0300

    Added some context implementations

---
+++ Dapper.Linq/Context/PostgresContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using System.Data.Common;
+using Dapper.Linq.Core;
+using Npgsql;
+
+namespace Dapper.Linq.Context
+{
+	public class PostgresContext : IStorageContext
+	{
+		internal DbConnectionStringBuilder Builder { get; }
+
+		public PostgresContext(string connection)
+		{
+			Builder = new NpgsqlConnectionStringBuilder(connection);
+		}
+
+		public IDbConnection ConfigureConnection(string login, string password)
+		{
+			var builder = new NpgsqlConnectionStringBuilder(
+				Builder.ConnectionString)
+			{
+				Username = login,
+				Password = password
+			};
+			
+			return new NpgsqlConnection(builder.ConnectionString);
+		}
+
+		public IDbConnection ConfigureConnection() =>
+			new NpgsqlConnection(Builder.ConnectionString);
+	}
+}
---
+++ Dapper.Linq/Context/SqlServerContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Dapper.Storage.Core;
+
+namespace Dapper.Storage.Context
+{
+	public class SqlServerContext : IStorageContext
+	{
+		internal DbConnectionStringBuilder Builder { get; }
+
+		public SqlServerContext(string connection)
+		{
+			Builder = new SqlConnectionStringBuilder(connection);
+		}
+
+		public IDbConnection ConfigureConnection(string login, string password)
+		{
+			var builder = new SqlConnectionStringBuilder(
+				Builder.ConnectionString)
+			{
+				UserID = login,
+				Password = password
+			};
+
+			return new SqlConnection(builder.ConnectionString);
+		}
+
+		public IDbConnection ConfigureConnection() =>
+			new SqlConnection(Builder.ConnectionString);
+	}
+}
