e2017b224225e992f0c6f03791acf3b1b942ce2b
Sasha <sashayudaev6@gmail.com>
2018-11-11 12:44:21 +0300

    Removed redundant namespaces

--- Dapper.Linq.Core/Mappers/PropertyMapper.cs
+++ Dapper.Linq.Core/Mappers/PropertyMapper.cs
@@ -1,33 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dapper.Linq.Core.Mappers
 {
 	public class PropertyMapper : IPropertyMapper
 	{
+		public PropertyInfo Property { get; }
+
 		public KeyType KeyType { get; private set; }
 		public string Name { get; private set; }
 		public string ColumnName { get; private set; }
-		public PropertyInfo Property { get; }
 
 		public PropertyMapper(PropertyInfo property)
 		{
 			Property = property;
 		}
 
+		public void Key(KeyType key)
+		{
+			KeyType = key;
+		}
 		public void Column(string column)
 		{
 			ColumnName = column ??
 				throw new ArgumentNullException(nameof(column));
-		}
-
-		public void Key(KeyType key)
-		{
-			KeyType = key;
 		}
 	}
 }
